8f83a2e621aa4d0a43910e5e3e7e50658de6db69
Pavitra Brahmbhatt <pavitrabrahmbhatt@Pavitras-MacBook-Air.local>
2019-08-22 12:45:51 -0500

    Register functionality

--- data-market/src/App.js
+++ data-market/src/App.js
@@ -1,18 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Login from './Login';
+import Register from './Register'
 
-
-function App() {
+const My404 = () =>{
   return (
-    <main>
-      <Switch>
-        <Route exact path="/" component={ Login } />
-        
-      </Switch>
-    </main>
-  );
+    <div>
+      You are lost
+    </div>
+  )
 }
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      full_name: '',
+      email: '',
+      loggedIn: false
+    }
+  }
+ 
+  register = async (data) => {
+
+     try {
+
+      const registerResponse = await fetch('http://localhost:8000/user/register', {
+        method: 'POST',
+        credentials: 'include', 
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json' 
+        }
+      })
+
+      const parsedResponse = await registerResponse.json();
+
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  render(){
+      return (
+        <main>
+          <Switch>
+            <Route exact path="/" render={(props) => <Login {...props} logIn={this.logIn} />} />
+            <Route exact path="/register" render={(props) => <Register {...props} register={this.register} /> } />
+            <Route component={My404} />
+          </Switch>
+        </main>
+    );
+  }
+}
+
 export default App;
