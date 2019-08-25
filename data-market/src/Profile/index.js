14d9c38a3b6a6ee57dc46e84e8b3534a1f42c187
Pavitra Brahmbhatt <pavitrabrahmbhatt@Pavitras-MacBook-Air.local>
2019-08-25 13:46:36 -0500

    Debugging

--- data-market/src/Profile/index.js
+++ data-market/src/Profile/index.js
@@ -3,9 +3,8 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PurchasedData from '../PurchasedData'
 import { Button, Form, Grid, Image, Message, Card, Icon} from 'semantic-ui-react';
-import { Menu, Container } from 'semantic-ui-react'
+import { Menu } from 'semantic-ui-react'
 import SoldData from '../SoldData'
-import EditData from '../EditData'
 
 //import Header from '../Header'
 
@@ -14,15 +13,7 @@ class Profile extends Component {
     super();
     this.state = {
       purchasedData: [],
-      soldData: [],
-      showEditModal: false,
-      dataToEditId: '',
-      name: '',
-      price: 0,
-      industry: '',
-      description: '',
-      territory: '',
-      source: ''
+      soldData: []
     }
   }
   componentDidMount() {
@@ -57,69 +48,11 @@ class Profile extends Component {
       return err
     }
   }
-  updateDataSet = (dataSetEdited) => {
-    const dataSetEditedArray = this.state.soldData.map(dataSet => {
-      if (dataSet.id === dataSetEdited.id) {
-        dataSet = dataSetEdited
-      }
-      return dataSet
-    })
-    this.setState({
-      soldData: dataSetEditedArray,
-      showEditModal: !this.state.showEditModal
-    })
-  }
-  closeModal = () => {
-    this.setState({
-      dataToEditId: '',
-      showEditModal: !this.state.showEditModal
-    })
-  }
-  showModal = (dataSet) => {
-    console.log(dataSet, "HERE IS THE DATASET SELECTED FOR EDIT")
-    this.setState({
-      dataToEditId: dataSet.id,
-      name: dataSet.name,
-      price: dataSet.price,
-      industry: dataSet.industry,
-      description: dataSet.description,
-      territory: dataSet.territory,
-      source: dataSet.source,
-      showEditModal: !this.state.showEditModal
-    })
-  }
-  deleteDataSet = async (dataSetId) => {
-    console.log(dataSetId, " id of data set to delete")
-
-    try {
-      const removeDataSet = await fetch(`http://localhost:8000/data/${dataSetId}`, {
-        method: 'Delete',
-        credentials: 'include'
-      })
-
-      if (removeDataSet.status !== 200) {
-        throw Error('delete request not working')
-      }
-      const removeDataSetJson = await removeDataSet.json()
-      console.log(removeDataSetJson)
-
-      this.setState({
-        soldData: this.state.soldData.filter((dataset) => dataset.id !== dataSetId)
-      })
-    } catch (err) {
-      console.log(err)
-      return err
-    }
-  }
 
   render(){
     return (
 
-<<<<<<< HEAD
       <Grid columns={4} padded style={{ height: '100vh'}}>
-=======
-      <Grid columns={3} padded style={{ height: '100vh'}}>
->>>>>>> 4164b127fdca32bd5cd2986f0ad4a70a60dc8506
         <Menu pointing secondary vertical>
             <Menu.Item as={ Link } to="">LOGO</Menu.Item>
             <Menu.Item as={ Link } to="/data/" >Browse Data</Menu.Item>
@@ -129,20 +62,11 @@ class Profile extends Component {
             <Menu.Item as={ Link } to="/">LogOut</Menu.Item>
         </Menu>
 
-<<<<<<< HEAD
+        
         Username: {this.props.userInfo.full_name}<Link to='/user/:id'>Edit Profile</Link>  <br/><br/>
         <PurchasedData orders={this.state.purchasedData}/> <br/>
 
         <SoldData soldData={this.state.soldData} /><Link to='/user/:id'>Edit</Link><Link to='/user/:id'>Delete</Link>
-=======
-        Username: {this.props.userInfo.full_name} <Link to='/user/:id'>Edit</Link> <br/><br/>
-        <Grid.Column>
-        <PurchasedData orders={this.state.purchasedData}/> <br/>
-        </Grid.Column>
-        <Grid.Column>
-        {this.state.showEditModal ? <EditData data={this.state} updateDataSet={this.updateDataSet} dataToEditId={this.state.dataToEditId} closeModal={this.closeModal}/> : <SoldData showModal={this.showModal} soldData={this.state.soldData} deleteDataSet={this.deleteDataSet}/>}
-        </Grid.Column>
->>>>>>> 4164b127fdca32bd5cd2986f0ad4a70a60dc8506
       </Grid>
       )
   }
