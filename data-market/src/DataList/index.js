2c5b513435ed2ab782a0e21cd204e1b00f89586d
Jordan Woods <jordan.woods@nlaw.northwestern.edu>
2019-08-26 12:37:14 -0500

    styling for feed

--- data-market/src/DataList/index.js
+++ data-market/src/DataList/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 //import EditEmployee from '../EditEmployee'
-import { Grid, Header, Card } from 'semantic-ui-react'
+import { Grid, Header, Card, Button } from 'semantic-ui-react'
 
 
 const DataSets = (props) => {
@@ -10,18 +10,25 @@ const DataSets = (props) => {
     return (
       <div key={i}>
         <Card>
-          <Card.Content header={dataset.name} />
-          <Card.Content>
+          <Card.Content header={dataset.name} style={{height: '50px'}} />
+          <Card.Content style={{height: '150px'}}>
           Description: {dataset.description}<br/>
           Industry: {dataset.industry}<br/>
           Posted: {dataset.created_at.toLocaleString()}<br/>
           Territory: {dataset.territory}
           </Card.Content>
           <Card.Content extra>
+          <div style={{display: 'flex', justifyContent: 'space-between'}}>
+            <div>
             <b>{dataset.price}</b>
+            </div>
+            <div>
+             <Button floated='right' data-product={i} onClick={props.showModal.bind(null, dataset.id)}>Purchase</Button>
+          </div>
+          </div>
           </Card.Content>
         </Card>
-        <button data-product={i} onClick={props.showModal.bind(null, dataset.id)}>Purchase</button>
+       
       </div>
     )
   })
@@ -39,11 +46,3 @@ const DataSets = (props) => {
 
 export default DataSets
 
-
-// <Grid.Column key={dataset._id}>
-//   <span><b>Title</b>: {dataset.name}</span><br/>
-//   <span><b>Price</b>: {dataset.price}</span><br/>
-//   <span><b>Industry</b>: {dataset.industry}</span><br/>
-//   <span><b>Description</b>: {dataset.description}</span><br/>
-//   <span><b>Territory</b>: {dataset.territory}</span><br/>
-// </Grid.Column>
